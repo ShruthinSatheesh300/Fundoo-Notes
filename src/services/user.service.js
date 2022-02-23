aa73c9b02760164a15117c9c85255570e0ee5439
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-02-23 11:57:38 +0530

    Initial commit

---
+++ src/services/user.service.js
@@ -0,0 +1,44 @@
+import User from '../models/user.model';
+import bcrypt from 'bcrypt';
+
+//get all users
+
+export const getAllUsers = async () => {
+  const data = await User.find();
+  return data;
+};
+
+//create new user
+export const userRegistration = async (body) => {
+  const saltRounds = 10;
+  const hasedPassword = bcrypt.hashSync(body.password, saltRounds);
+  body.password = hasedPassword;
+  const data = await User.create(body);
+  return data;
+};
+
+//update single user
+export const updateUser = async (_id, body) => {
+  const data = await User.findByIdAndUpdate(
+    {
+      _id
+    },
+    body,
+    {
+      new: true
+    }
+  );
+  return data;
+};
+
+//delete single user
+export const deleteUser = async (id) => {
+  await User.findByIdAndDelete(id);
+  return '';
+};
+
+//get single user
+export const getUser = async (id) => {
+  const data = await User.findById(id);
+  return data;
+};
