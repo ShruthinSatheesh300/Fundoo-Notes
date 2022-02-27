2cfa8e8f05d1d59d253f71ee75bc01331e208cce
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-02-27 21:08:19 +0530

    [Shruthin] Refactored : Removed non-essential APIs

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,14 +1,7 @@
-/* eslint-disable prettier/prettier */
 import User from '../models/user.model';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
-//get all users
-
-export const getAllUsers = async () => {
-  const data = await User.find();
-  return data;
-};
 
 //create new user
 export const userRegistration = async (body) => {
@@ -26,38 +19,11 @@ export const login = async (body) => {
   } else {
     const validPassword = bcrypt.compareSync(body.password, user.password);
     if (validPassword) {
-
-      const token = jwt.sign({'email': user.email,'id':user._id},
-      process.env.SECRET_CODE);
-       return token;
+      const token = jwt.sign({ 'email': user.email, 'id': user._id },
+        process.env.SECRET_CODE);
+      return token;
     } else {
       throw new Error('password is invalid');
     }
   }
-};
-
-//update single user
-export const updateUser = async (_id, body) => {
-  const data = await User.findByIdAndUpdate(
-    {
-      _id
-    },
-    body,
-    {
-      new: true
-    }
-  );
-  return data;
-};
-
-//delete single user
-export const deleteUser = async (id) => {
-  await User.findByIdAndDelete(id);
-  return '';
-};
-
-//get single user
-export const getUser = async (id) => {
-  const data = await User.findById(id);
-  return data;
 };
