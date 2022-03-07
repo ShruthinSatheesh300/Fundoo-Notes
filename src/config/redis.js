618da574a5b8dc55a144827fef4c7202020f60e8
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-03-07 23:12:50 +0530

    [Shruthin] Added Redis source code for connection

---
+++ src/config/redis.js
@@ -0,0 +1,14 @@
+import { createClient } from 'redis';
+import logger from './logger';
+
+export const client = createClient();
+
+const clientRedis = async() => {
+  
+    try {
+        await client.connect();    
+    } catch (error) {
+        console.log(error);
+    }
+}
+export default clientRedis;
