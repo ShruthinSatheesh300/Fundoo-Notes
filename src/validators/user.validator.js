44ffada19d91523f9c173cf24ec2a19bd22c7a28
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-03-09 23:23:22 +0530

    [Shruthin] Reffactored some Validation errors

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -1,5 +1,5 @@
 import Joi from '@hapi/joi';
-
+import HttpStatus from 'http-status-codes'
 export const newUserValidator = (req, res, next) => {
   const schema = Joi.object({
     firstName: Joi.string().min(4).required().error( Error('Enter a appropriate first name')),
@@ -9,7 +9,9 @@ export const newUserValidator = (req, res, next) => {
   });
   const { error, value } = schema.validate(req.body);
   if (error) {
-    next(error);
+    res.status(HttpStatus.BAD_REQUEST).json({
+    code: HttpStatus.BAD_REQUEST,
+    message: `Enter valid deatils : ${error}`})
   } else {
     req.validatedBody = value;
     next();
